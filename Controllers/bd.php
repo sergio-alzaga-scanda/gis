9faf1a9ad2b6e939d82015d7e5d1664bce8c8d23
Sergio Alzaga <sergio.alzaga@scanda.com.mx>
2025-06-27 17:38:58 -0600

    Mod

--- Controllers/bd.php
+++ Controllers/bd.php
@@ -1,33 +1,35 @@
 <?php
 // Datos de conexión
-$servername = "localhost"; // Nombre del servidor
-$port = 3307;              // Puerto MySQL personalizado
-$username = "root";        // Nombre de usuario
-$password = "";        // Contraseña del usuario
-$database = "gis";         // Nombre de la base de datos
+//$servername = "localhost"; // Nombre del servidor
+//$port = 3307;              // Puerto MySQL personalizado
+//$username = "root";        // Nombre de usuario
+//$password = "";        // Contraseña del usuario
+//$database = "gis";         // Nombre de la base de datos
+//
+//// Crear conexión con puerto
+//$conn = new mysqli($servername, $username, $password, $database, $port);
+//
+//// Verificar conexión
+//if ($conn->connect_error) {
+//    die("Conexión fallida: " . $conn->connect_error);
+//}
 
-// Crear conexión con puerto
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "Melco154.,";
+$database = "gis_db";
+$port = 3306;
+
 $conn = new mysqli($servername, $username, $password, $database, $port);
 
-// Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8");
-
-
-//$servername = "localhost";
-//$username = "root";
-//$password = "Melco154.,";
-//$database = "gis_db";
-//$port = 3306;
-//
-//$conn = new mysqli($servername, $username, $password, $database, $port);
-//
-//if ($conn->connect_error) {
-//    die("Conexión fallida: " . $conn->connect_error);
-//}
 ?>
 
 
