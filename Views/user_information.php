aa847bb94b52c905364ecc9555d3146e2caae027
Sergio Alzaga <sergio.alzaga@scanda.com.mx>
2025-06-30 10:54:10 -0600

    Mod

--- Views/user_information.php
+++ Views/user_information.php
@@ -567,15 +567,5 @@ function cargarDirectorio(localidad) {
 }
 
 </script>
-<script>
-$(document).ready(function() {
-    $('#tablaResultadosCategorias').DataTable({
-        responsive: true,
-        language: {
-            url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json"
-        }
-    });
-});
-</script>
 </body>
 </html>
