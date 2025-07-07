5d4d8dad59793973204a2ca8724ccea34b5d1f4e
Sergio Alzaga <sergio.alzaga@scanda.com.mx>
2025-07-06 18:14:00 -0600

    Mod

--- Views/user_information.php
+++ Views/user_information.php
@@ -639,5 +639,13 @@ function cargarDirectorio(localidad) {
 
 </script>
 
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/app.js"></script>
+
 </body>
 </html>
