c166046943425dd51457ca45a4fceebcb67757f8
Sergio Alzaga <sergio.alzaga@scanda.com.mx>
2025-07-06 18:19:40 -0600

    Mod

--- Views/user_information.php
+++ Views/user_information.php
@@ -24,7 +24,14 @@ if ($localidad !== '') {
 <!-- Buttons CSS -->
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css" />
 
-
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<!-- Buttons JS -->
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
 <!-- Dependencias para exportar Excel y PDF -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
@@ -584,7 +591,9 @@ function cargarResponsables(localidad) {
                     $('#tablaResponsables').DataTable().destroy();
                 }
                 $('#tablaResponsables').DataTable({
-    
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
+    },
     dom: 'Bfrtip', // <- Esto es clave para que se muestren los botones
     buttons: ['copy', 'csv', 'excel', 'print'],
     responsive: true
@@ -617,7 +626,9 @@ function cargarDirectorio(localidad) {
                 // Inicializar DataTable
                
                 $('#tablaDirectorio').DataTable({
-    
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
+    },
     dom: 'Bfrtip', // <- Esto es clave para que se muestren los botones
     buttons: ['copy', 'csv', 'excel', 'print'],
     responsive: true
@@ -638,14 +649,6 @@ function cargarDirectorio(localidad) {
 
 
 </script>
-
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../assets/js/app.js"></script>
 
 </body>
 </html>
