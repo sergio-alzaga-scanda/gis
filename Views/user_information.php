551d922597f1ac11885190e006f9e990b39ed666
Sergio Alzaga <sergio.alzaga@scanda.com.mx>
2025-07-14 12:47:40 -0600

    Mod

--- Views/user_information.php
+++ Views/user_information.php
@@ -628,5 +628,21 @@ function cargarDirectorio(localidad) {
 }
 
 </script>
+<script type="text/javascript">
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '6871769203c03c9815ef9590' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
 </body>
 </html>
